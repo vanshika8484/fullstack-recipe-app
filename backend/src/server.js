ff500aab9047c03c5fdf70fb151ad524086c4be2
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-02-09 15:13:21 +0530

    cron.js added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,8 +3,13 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favorites } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 const app=express();
 const PORT=ENV.PORT;
+
+if(ENV.NODE_ENV==="production"){
+    job.start();
+}
 app.use(express.json());
 app.get("/api/health",(req,res)=>{
     res.status(200).json({success:true});
