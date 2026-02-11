eaefddf0001a0d24ea4bc14276eb1dc07dd05f11
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-02-11 19:03:49 +0530

    Building sign-in screen

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -2,11 +2,11 @@ import { Slot, Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
-
+import { COLORS} from '../constants/colors'
 export default function RootLayout() {
   return (
      <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1,backgroundColor:COLORS.background }}>
         <Slot />
       </SafeAreaView>
      </ClerkProvider>
