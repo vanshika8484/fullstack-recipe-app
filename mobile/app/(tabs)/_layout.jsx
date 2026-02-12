b3cdbba5f96091dc8d4c8741fec094fc8714f361
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-02-12 18:30:08 +0530

    Setting Up Tab Navigator

--- mobile/app/(tabs)/_layout.jsx
+++ mobile/app/(tabs)/_layout.jsx
@@ -1,5 +1,7 @@
-import { Redirect, Stack } from "expo-router";
+import { Redirect, Stack, Tabs } from "expo-router";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants/colors";
 
 export default function TabLayout() {
     const {isSignedIn} = useAuth();
@@ -7,6 +9,26 @@ export default function TabLayout() {
         return <Redirect href="/(auth)/sign-in" />
     }
   return (
-    <Stack />   
+    <Tabs screenOptions={{headerShown: false,
+      tabBarActiveTintColor: COLORS.primary,
+      tabBarInactiveTintColor: COLORS.textLight,
+      tabBarStyle: {
+        backgroundColor: COLORS.white,
+        borderTopColor: COLORS.border,
+        borderTopWidth:1,
+        height:80,
+        paddingBottom:8,
+        paddingTop:8,
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        fontWeight: "600",
+      },
+      
+    }}> 
+      <Tabs.Screen name="index" options={{title: "Recipes",tabBarIcon:({color,size})=><Ionicons name="restaurant" size={size} color={color} />}} />
+      <Tabs.Screen name="search" options={{title: "Search",tabBarIcon:({color,size})=><Ionicons name="search" size={size} color={color} />}} />
+      <Tabs.Screen name="favorites" options={{title: "Favorites",tabBarIcon:({color,size})=><Ionicons name="heart" size={size} color={color} />}} />
+    </Tabs>
   );
 }
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,14 +1,14 @@
 import { Slot, Stack } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { COLORS} from '../constants/colors'
+import SafeScreen from '../components/SafeScreen'
 export default function RootLayout() {
   return (
      <ClerkProvider tokenCache={tokenCache}>
-      <SafeAreaView style={{ flex: 1,backgroundColor:COLORS.background }}>
+     <SafeScreen>
         <Slot />
-      </SafeAreaView>
+     </SafeScreen>
      </ClerkProvider>
   )
 }
