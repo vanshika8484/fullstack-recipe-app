aa570b9cc1ecaeb52cf39bbad50763e75024ff2b
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-02-18 22:13:28 +0530

    create home screen

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,11 +1,32 @@
+// import { Slot, Stack } from "expo-router";
+// import { ClerkProvider } from '@clerk/clerk-expo'
+// import { tokenCache } from '@clerk/clerk-expo/token-cache'
+// import { COLORS} from '../constants/colors'
+// import SafeScreen from '../components/SafeScreen'
+// export default function RootLayout() {
+//   return (
+//      <ClerkProvider tokenCache={tokenCache}>
+//      <SafeScreen>
+//         <Slot />
+//      </SafeScreen>
+//      </ClerkProvider>
+//   )
+// }
 import { Slot, Stack } from "expo-router";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import { COLORS} from '../constants/colors'
 import SafeScreen from '../components/SafeScreen'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY
+
 export default function RootLayout() {
+  if (!publishableKey) {
+    throw new Error('Missing EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY')
+  }
+
   return (
-     <ClerkProvider tokenCache={tokenCache}>
+     <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
      <SafeScreen>
         <Slot />
      </SafeScreen>
